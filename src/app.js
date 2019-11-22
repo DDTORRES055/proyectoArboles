3f2cbae4be8e892931acd465b1ebfcae4f6005bf
Daniel Díaz Torres <ddtorres055@gmail.com>
2019-11-22 01:16:54 -0600

    Poner arbol

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 var cy = cytoscape({
 
     container: document.getElementById('container'), // container to render in
@@ -6,21 +7,15 @@ var cy = cytoscape({
 
     elements: {
       nodes: [
-        { data: { id: 'j', name: 'Jerry' } },
+        { data: { id: 'a', name: 'Ana' }, },
         { data: { id: 'e', name: 'Elaine' } },
         { data: { id: 'k', name: 'Kramer' } },
         { data: { id: 'g', name: 'George' } }
       ],
       edges: [
-        { data: { source: 'j', target: 'e' } },
-        { data: { source: 'j', target: 'k' } },
-        { data: { source: 'j', target: 'g' } },
-        { data: { source: 'e', target: 'j' } },
-        { data: { source: 'e', target: 'k' } },
-        { data: { source: 'k', target: 'j' } },
-        { data: { source: 'k', target: 'e' } },
-        { data: { source: 'k', target: 'g' } },
-        { data: { source: 'g', target: 'j' } }
+        { data: { source: 'a', target: 'e' } },
+        { data: { source: 'a', target: 'k' } },
+        { data: { source: 'e', target: 'g' } }
       ]
     },
 
@@ -97,15 +92,14 @@ var cy = cytoscape({
     ],
 
     layout: {
-      name: 'grid',
-      rows: 2,
-      cols: 2
+      name: 'dagre'
     }
   
   });
 
   cy.edgehandles();
   
+  var Nodes = cy.json().elements.nodes;
 
   function addNode(){
     cy.add({
@@ -122,4 +116,111 @@ var cy = cytoscape({
       { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
     ]);
     console.log(eles);
+  }
+
+  cy.on('taphold', function(evt){
+    var element = evt.target;
+    cy.remove(element);
+
+    Nodes = cy.json().elements.nodes;
+
+
+    if(Nodes == undefined){
+      clear();
+    }
+  });
+
+
+  function clear() {
+
+    cy = cy = cytoscape({
+
+      container: document.getElementById('container'), // container to render in
+
+      elements: {
+        nodes: [],
+        edges: []
+      },
+  
+      style: [
+        {
+          selector: 'node[name]',
+          style: {
+            'content': 'data(name)'
+          }
+        },
+  
+        {
+          selector: 'edge',
+          style: {
+            'curve-style': 'bezier',
+            'target-arrow-shape': 'triangle'
+          }
+        },
+  
+        // some style for the extension
+  
+        {
+          selector: '.eh-handle',
+          style: {
+            'background-color': 'red',
+            'width': 12,
+            'height': 12,
+            'shape': 'ellipse',
+            'overlay-opacity': 0,
+            'border-width': 12, // makes the handle easier to hit
+            'border-opacity': 0
+          }
+        },
+  
+        {
+          selector: '.eh-hover',
+          style: {
+            'background-color': 'red'
+          }
+        },
+  
+        {
+          selector: '.eh-source',
+          style: {
+            'border-width': 2,
+            'border-color': 'red'
+          }
+        },
+  
+        {
+          selector: '.eh-target',
+          style: {
+            'border-width': 2,
+            'border-color': 'red'
+          }
+        },
+  
+        {
+          selector: '.eh-preview, .eh-ghost-edge',
+          style: {
+            'background-color': 'red',
+            'line-color': 'red',
+            'target-arrow-color': 'red',
+            'source-arrow-color': 'red'
+          }
+        },
+  
+        {
+          selector: '.eh-ghost-edge.eh-preview-active',
+          style: {
+            'opacity': 0
+          }
+        }
+      ],
+  
+      layout: {
+        name: 'grid',
+        rows: 2,
+        cols: 2
+      }
+    
+    });
+
+    cy.edgehandles();
   }
