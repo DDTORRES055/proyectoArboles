64839f87a85994005821160bf35c9680bcbcf982
Daniel Díaz Torres <ddtorres055@gmail.com>
2019-11-21 21:43:50 -0600

    Dibujar ramas

--- src/app.js
+++ src/app.js
@@ -2,41 +2,124 @@ var cy = cytoscape({
 
     container: document.getElementById('container'), // container to render in
   
-    elements: [ // list of graph elements to start with
-      { // node a
-        data: { id: 'a' }
-      },
-      { // node b
-        data: { id: 'b' }
-      },
-      { // edge ab
-        data: { id: 'ab', source: 'a', target: 'b' }
-      }
-    ],
-  
-    style: [ // the stylesheet for the graph
+    
+
+    elements: {
+      nodes: [
+        { data: { id: 'j', name: 'Jerry' } },
+        { data: { id: 'e', name: 'Elaine' } },
+        { data: { id: 'k', name: 'Kramer' } },
+        { data: { id: 'g', name: 'George' } }
+      ],
+      edges: [
+        { data: { source: 'j', target: 'e' } },
+        { data: { source: 'j', target: 'k' } },
+        { data: { source: 'j', target: 'g' } },
+        { data: { source: 'e', target: 'j' } },
+        { data: { source: 'e', target: 'k' } },
+        { data: { source: 'k', target: 'j' } },
+        { data: { source: 'k', target: 'e' } },
+        { data: { source: 'k', target: 'g' } },
+        { data: { source: 'g', target: 'j' } }
+      ]
+    },
+
+    style: [
       {
-        selector: 'node',
+        selector: 'node[name]',
         style: {
-          'background-color': '#666',
-          'label': 'data(id)'
+          'content': 'data(name)'
         }
       },
-  
+
       {
         selector: 'edge',
         style: {
-          'width': 3,
-          'line-color': '#ccc',
-          'target-arrow-color': '#ccc',
+          'curve-style': 'bezier',
           'target-arrow-shape': 'triangle'
+        }
+      },
+
+      // some style for the extension
+
+      {
+        selector: '.eh-handle',
+        style: {
+          'background-color': 'red',
+          'width': 12,
+          'height': 12,
+          'shape': 'ellipse',
+          'overlay-opacity': 0,
+          'border-width': 12, // makes the handle easier to hit
+          'border-opacity': 0
+        }
+      },
+
+      {
+        selector: '.eh-hover',
+        style: {
+          'background-color': 'red'
+        }
+      },
+
+      {
+        selector: '.eh-source',
+        style: {
+          'border-width': 2,
+          'border-color': 'red'
+        }
+      },
+
+      {
+        selector: '.eh-target',
+        style: {
+          'border-width': 2,
+          'border-color': 'red'
+        }
+      },
+
+      {
+        selector: '.eh-preview, .eh-ghost-edge',
+        style: {
+          'background-color': 'red',
+          'line-color': 'red',
+          'target-arrow-color': 'red',
+          'source-arrow-color': 'red'
+        }
+      },
+
+      {
+        selector: '.eh-ghost-edge.eh-preview-active',
+        style: {
+          'opacity': 0
         }
       }
     ],
-  
+
     layout: {
       name: 'grid',
-      rows: 1
+      rows: 2,
+      cols: 2
     }
   
   });
+
+  cy.edgehandles();
+  
+
+  function addNode(){
+    cy.add({
+      group: 'nodes',
+      data: { id: 'h', name: 'Hola', weight: 75 },
+      position: { x: 200, y: 200 }
+  });
+  }
+
+  function addNodes(){
+    var eles = cy.add([
+      { group: 'nodes', data: { id: 'n0' }, position: { x: 100, y: 100 } },
+      { group: 'nodes', data: { id: 'n1' }, position: { x: 200, y: 200 } },
+      { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
+    ]);
+    console.log(eles);
+  }
